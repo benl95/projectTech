244620b85bef75103eab5a18dbcc032c9dd84ba5
benl95 <beno_1995@hotmail.com>
2020-02-28 21:45:37 +0100

    Added annotations to index.js

--- index.js
+++ index.js
@@ -1,40 +1,31 @@
 const express = require('express');
-
 const app = express();
-const handlebars = require('express-handlebars');
+const expbs = require('express-handlebars');
+const path = require('path');
 
 // Handlebars setup
+app.engine('handlebars', expbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(__dirname, 'views/layouts')
+}));
 app.set('view engine', 'handlebars');
 
-app.engine('handlebars', handlebars({
-    layoutsDir: __dirname + '/views/layouts',
-}));
+// Static folders 
+app.use(express.static(path.join(__dirname, '/public')));
 
+// Routing
 app.get('/', (req, res) => {
-    res.render('main', {
-        layout: 'index'
+    res.render('index', {
+        title: 'Seek matches'
     });
 });
 
-// Routing
-// app.use(express.static('public'));
+app.get('/playlist', (req, res) => {
+    res.render('playlist', {
+        title: 'Results'
+    });
+});
 
-// app.get('/about', (req, res) => {
-//     res.send('About page');
-// });
-
-// app.get('/', (req, res) => {
-//     res.render('index', {
-//         layout: false,
-//     });
-// });
-
-// app.get('/contact', (req, res) => {
-//     res.send('Contact page');
-// });
-
-// Port
-const PORT = process.env.PORT || 5000;
-
-// app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-app.listen(PORT, () => console.log(`App listening to port ${PORT}`));
+app.listen(8080, () => {
+    console.log('Server is starting on port ', 8080);
+});
