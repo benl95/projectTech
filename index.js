36d686d32b91073891af6d3ceb63b76f62699cdc
benl95 <beno_1995@hotmail.com>
2020-02-28 18:36:31 +0100

    Installed express-handlebars and set up the handlebars template engine

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
 const express = require('express');
+
 const app = express();
-const hbs = require('handlebars');
+const handlebars = require('express-handlebars');
 
-// View engine setup
-// app.set('view engine', 'handlebars');
-app.set('view engine', 'hbs');
+// Handlebars setup
+app.set('view engine', 'handlebars');
 
-// Routing
-app.use(express.static('public'));
-
-app.get('/about', (req, res) => {
-    res.send('About page');
-});
+app.engine('handlebars', handlebars({
+    layoutsDir: __dirname + '/views/layouts',
+}));
 
 app.get('/', (req, res) => {
-    res.render('index', {
-        layout: false
-    })
-})
-
-app.get('/contact', (req, res) => {
-    res.send('Contact page');
+    res.render('main', {
+        layout: 'index'
+    });
 });
+
+// Routing
+// app.use(express.static('public'));
+
+// app.get('/about', (req, res) => {
+//     res.send('About page');
+// });
+
+// app.get('/', (req, res) => {
+//     res.render('index', {
+//         layout: false,
+//     });
+// });
+
+// app.get('/contact', (req, res) => {
+//     res.send('Contact page');
+// });
 
 // Port
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`App listening to port ${PORT}`));
