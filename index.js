85b3f26f7943b999f0a8620d90c61404fa3a6f30
benl95 <beno_1995@hotmail.com>
2020-03-12 13:30:02 +0100

    Made the MongoDB connection setup

--- index.js
+++ index.js
@@ -3,6 +3,43 @@ const app = express();
 const expbs = require('express-handlebars');
 const path = require('path');
 const bodyParser = require('body-parser');
+const {
+    MongoClient
+} = require('mongodb');
+require('dotenv').config()
+
+// MongoDB connection setup
+async function main() {
+
+    const uri = 'mongodb+srv://admin:' + process.env.DB_PASS '@projecttech-a3phf.mongodb.net/test';
+
+
+    const client = new MongoClient(uri);
+
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+
+        // Make the appropriate DB calls
+        await listDatabases(client);
+
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+
+// Database function retrieve list databases
+
+async function listDatabases(client) {
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
 
 // Handlebars setup
 app.engine('handlebars', expbs({
